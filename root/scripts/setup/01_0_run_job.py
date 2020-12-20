a565e49c9f43fb019d69743051d7b474cb7abc83
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2020-12-19 22:16:00 -0600

    generalize backup

--- root/scripts/setup/01_0_run_job.py
+++ root/scripts/setup/01_0_run_job.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python3
 
 # System Imports
+import distutils.log
+
 from os import getenv
 from pathlib import PurePath
 from tarfile import open as tar_open
 from shutil import move
 from datetime import datetime
-
-# 3rd Party Imports
-from mcrcon import MCRcon
+from distutils.dir_util import copy_tree
+from distutils.dir_util import remove_tree
+from logging import INFO
 
 # Local Imports
 from python_logger import create_logger
 
 def main():
-  logger = create_logger(PurePath(__file__).stem)
+    logger = create_logger(PurePath(__file__).stem)
 
-  date_stamp = datetime.now().strftime("%G-W%V-%u-%H-%M-%S")
-  backup_name = f'minecraft-vanillaplusplus-backup-{date_stamp}.tar.xz'
-  create_backup_path = f'/mnt/{backup_name}'
-  backup_path = '/mnt/minecraft/world/.'
-  final_backup_destination = f'/mnt/backups/{backup_name}'
+    logger.setLevel(INFO)
+    distutils.log = logger
 
-  with MCRcon("192.168.8.3", "rcon") as mcr:
-    resp = mcr.command("save-off")
-    logger.info(resp)
-    resp = mcr.command("save-all")
-    logger.info(resp)
+    base_backup_name = getenv('BACKUP_NAME','minecraft-vanillaplusplus-backup')
+
+    date_stamp = datetime.now().strftime("%G-W%V-%u-%H-%M-%S")
+    backup_name = f'{base_backup_name}-{date_stamp}.tar.xz'
+    create_backup_path = f'/mnt/{backup_name}'
+    backup_path = '/mnt/source/'
+    final_backup_destination = f'/mnt/backups/{backup_name}'
+    temp_path = '/mnt/tmp/'
+
+    remove_tree(temp_path)
+
+    copy_tree(
+      backup_path,
+      temp_path
+    )
 
     logger.info('Creating Backup')
 
     with tar_open(create_backup_path, 'w:xz') as tar:
-      tar.add(backup_path, arcname='')
+        tar.add(f'{temp_path}.', arcname='')
 
-    resp = mcr.command("save-on")
-    logger.info(resp)
+    logger.info('Moving Backup')
+    move(create_backup_path, final_backup_destination)
 
-  logger.info('Moving Backup')
-  move(create_backup_path, final_backup_destination)
-
-  logger.info(f'Successfully executed cron job')
+    logger.info('Successfully executed cron job')
 
 if __name__ == '__main__':
-  main()
+    main()
